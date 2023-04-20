b8049f45001df23f43140709532ca62854a02fd1
MJ Rossetti <s2t2@users.noreply.github.com>
2023-04-20 11:40:01 -0400

    Pass other variables to the page

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -28,4 +28,4 @@ def hello_world():
     message = f"Hello, {name}!"
 
     #return message
-    return render_template("hello.html", message=message)
+    return render_template("hello.html", message=message, other="YEAH", x=5)
